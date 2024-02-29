1b73fc2e67bcc634a3f69f97f6856c8e40d1187b
Sebchav <schavarria13@alumnos.uaq.mx>
2024-02-29 10:54:34 -0600

    CarCard Component

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,11 +1,36 @@
 "use state"
 import {useState} from 'react'
 import Image from 'next/image'
+import { CustomButton } from '.';
+import { CarProps } from '@/types'
+import { calculateCarRent } from '@/utils';
 
-const CarCard = ({car}) => {
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({car}: CarCardProps )=> {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
+  const carRent = calculateCarRent(city_mpg, year);
+
   return (
-    <div>
-      CarCard
+    <div className='car-card group'>
+      <div className="car_card__content">
+        <h2 className='car-card__content-title'>
+          {make} {model}
+        </h2>
+      </div>
+
+      <p className='flex mt-6 text-[32px] font-extrabold'>
+        <span className='self-start text-[14px] font-semibold'>
+          $
+        </span>
+        {carRent} 
+        <span className='self-end text-[14px] font-medium'>
+          /day
+        </span>
+      </p>
     </div>
   )
 }
