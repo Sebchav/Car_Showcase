d756a2a0777e670a61199e96cc38dd08c7480bc5
Sebchav <schavarria13@alumnos.uaq.mx>
2024-03-05 10:14:36 -0600

    Project finished

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -7,15 +7,9 @@ import { Listbox, Transition } from '@headlessui/react';
 import { CustomFilterProps } from '@/types';
 import { updateSearchParams } from '@/utils';
 
-const CustomFilter = ({title, options}:CustomFilterProps) => {
-  const router = useRouter();
+const CustomFilter = ({title, options, setFilter}:CustomFilterProps) => {
+
   const [ selected, setSelected ] = useState(options[0]);
-
-  const handleUpdateParams = (e: {title: string, value: string})=> {
-    const newPathName  = updateSearchParams(title, e.value.toLowerCase());
-
-    router.push(newPathName)
-  }
 
   return (
     <div
@@ -25,7 +19,7 @@ const CustomFilter = ({title, options}:CustomFilterProps) => {
         value={selected}
         onChange={(e)=> {
           setSelected(e);
-          handleUpdateParams(e)
+          setFilter(e.value);
         }}
       >
         <div
